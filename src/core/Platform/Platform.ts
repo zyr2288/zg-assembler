b571c6a10a82df2a698b2656935c2a32e0458d7e
844195652@qq.com <844195652@qq.com>
2022-06-14 21:54:23 +0800

    添加65816平台

--- src/core/Platform/Platform.ts
+++ src/core/Platform/Platform.ts
@@ -1,5 +1,5 @@
-import { MyException } from "../Base/MyException";
 import { Asm6502 } from "./Asm6502";
+import { Asm65816 } from "./Asm65816";
 import { InstructionBase } from "./InstructionBase";
 
 export class Platform {
@@ -10,6 +10,9 @@ export class Platform {
 		switch(platform) {
 			case "6502":
 				this.instructionAnalyser = new Asm6502();
+				break;
+			case "65816":
+				this.instructionAnalyser = new Asm65816();
 				break;
 			default:
 				console.error("不支持平台" + platform);
