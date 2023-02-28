7f051f94d6db510237bf80949b70992f9645da8a
844195652@qq.com <844195652@qq.com>
2023-02-28 21:38:07 +0800

    更新6502指令

--- src/Core/Platform/Asm6502.ts
+++ src/Core/Platform/Asm6502.ts
@@ -30,7 +30,8 @@ export class Asm6502 extends AsmCommon {
 
 		const ins2 = [
 			"LDA", "LDX", "LDY", "STA", "STX", "STY",
-			"ADC"
+			"ADC", "SBC", "AND", "EOR", "ORA",
+			"CMP", "CPX", "CPY"
 		];
 
 		const addressingMode2 = [
@@ -43,9 +44,15 @@ export class Asm6502 extends AsmCommon {
 			   [0xA2],      null,          [0xB6, 0xBE],    null,          null,         [0xA6, 0xAE],          //LDX
 			   [0xA0],     [0xB4, 0xBC],    null,           null,          null,         [0xA4, 0xAC],          //LDY
 			    null,      [0x95, 0x9D],   [-1, 0x99],     [0x81],        [0x91],        [0x85, 0x8D],          //STA
-			    null,       null,          [0x96],         [0x86, 0x8E],   null,          null,                 //STX
-			    null,      [0x94],          null,           null,          null,         [0x84, 0x8C]           //STY
-			   [0x69],     [0x75, 0x7D],   [-1, 0x79]
+			    null,       null,          [0x96],          null,          null,          null,                 //STX
+			    null,      [0x94],          null,           null,          null,         [0x84, 0x8C],          //STY
+			   [0x69],     [0x75, 0x7D],   [-1, 0x79],     [0x61],        [0x71],        [0x65, 0x6D],          //ADC
+			   [0xE9],     [0xF5, 0xFD],   [-1, 0xF9],     [0xE1],        [0xF1],        [0xE5, 0xED],          //SBC
+			   [0x29],     [0x35, 0x3D],   [-1, 0x39],     [0x21],        [0x31],        [0x25, 0x2D],          //AND
+			   [0x49],     [0x55, 0x5D],   [-1, 0x59],     [0x41],        [0x51],        [0x45, 0x4D],          //EOR
+			   [0x09],     [0x15, 0x1D],   [-1, 0x19],     [0x01],        [0x11],        [0x05, 0x0D],          //ORA
+			   [0xC9],     [0xD5, 0xDD],   [-1, 0xD9],     [0xC1],        [0xD1],        [0xC5, 0xCD],          //CMP
+			   [0xE0],      null,           null,           null,          null,         [0xE4, 0xEC],          //CPX
 		];
 
 	}
