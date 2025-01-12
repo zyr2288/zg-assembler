cf44a7bdfc2f7614dfc97ccb9a90f0d84d53b2cf
844195652@qq.com <844195652@qq.com>
2025-01-13 01:35:46 +0800

    添加web的编译

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -88,6 +88,45 @@ const coreBuilderNode = {
 	},
 }
 
+/** @type WebpackConfig */
+const coreBuilderES6 = {
+	target: ["web"],
+	mode: "production",
+	entry: "./src/Core/ZGAssembler.ts",
+	output: {
+		// the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+		path: path.resolve(__dirname, buildCore),
+		filename: `core-web-${packageJson.version}.js`,
+		library: { name: "ZGAssembler", type: "window", export: "ZGAssembler" },
+	},
+	externals: {
+		vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+		// modules added here also need to be added in the .vscodeignore file
+	},
+	resolve: {
+		// support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+		extensions: ['.ts', '.js']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'ts-loader',
+					options: {
+						configFile: "tsconfig.node.json"
+					}
+				}]
+			}
+		]
+	},
+	devtool: 'nosources-source-map',
+	infrastructureLogging: {
+		level: "log", // enables logging required for problem matchers
+	},
+}
+
 module.exports = [
-	coreBuilderCS, coreBuilderNode
+	coreBuilderCS, coreBuilderNode, coreBuilderES6
 ];
