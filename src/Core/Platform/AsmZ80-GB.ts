b9c943c66caf09c73e3589154fef3dfbc3dda015
844195652@qq.com <844195652@qq.com>
2025-12-23 02:18:27 +0800

    更新z80-gb

--- src/Core/Platform/AsmZ80-GB.ts
+++ src/Core/Platform/AsmZ80-GB.ts
@@ -1,6 +1,11 @@
 import { IAsmPlatform } from "./IAsmPlatform";
 import { AddInstructionOption, Platform } from "./Platform";
 
+/**
+ * z80-gb的指令表
+ * 
+ * https://gbdev.io/pandocs/CPU_Registers_and_Flags.html
+ */
 export class AsmZ80_GB implements IAsmPlatform {
 
 	platformName = "z80-gb";
@@ -10,10 +15,10 @@ export class AsmZ80_GB implements IAsmPlatform {
 	}
 
 	private Initialize() {
-		let addresssingMode, opCode;
+		let addressingMode, opCode1, opCode2;
 
-		addresssingMode = ["A", "B", "C", "D", "E", "H", "L", "(HL)"];
-		opCode = [
+		addressingMode = ["A", "B", "C", "D", "E", "H", "L", "(HL)"];
+		opCode1 = [
 			// A    B     C     D     E     H     L    (HL)
 			[0x7F, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E],		//A
 			[0x47, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46],		//B
@@ -23,17 +28,17 @@ export class AsmZ80_GB implements IAsmPlatform {
 			[0x67, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66],		//H
 			[0x6F, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E],		//L
 		];
-		for (let i = 0; i < addresssingMode.length - 1; ++i) {
-			for (let j = 0; j < addresssingMode.length; ++j) {
-				let mode = `${addresssingMode[i]},${addresssingMode[j]}`;
-				Platform.AddInstruction("LD", { addressingMode: mode, opCode: [opCode[i][j]] });
+		for (let i = 0; i < addressingMode.length - 1; ++i) {
+			for (let j = 0; j < addressingMode.length; ++j) {
+				const mode = `${addressingMode[i]},${addressingMode[j]}`;
+				Platform.AddInstruction("LD", { addressingMode: mode, opCode: [opCode1[i][j]] });
 			}
 		}
 
-		addresssingMode = "BCDEHL";
-		opCode = [0x06, 0x0E, 0x16, 0x1E, 0x26, 0x2E];
-		for (let i = 0; i < addresssingMode.length; ++i)
-			Platform.AddInstruction("LD", { addressingMode: `${addresssingMode[i]},[exp]`, opCode: [, opCode[i]] });
+		addressingMode = "BCDEHL";
+		opCode1 = [0x06, 0x0E, 0x16, 0x1E, 0x26, 0x2E];
+		for (let i = 0; i < addressingMode.length; ++i)
+			Platform.AddInstruction("LD", { addressingMode: `${addressingMode[i]},[exp]`, opCode: [, opCode1[i]] });
 
 		this.A_AddressingMode(["(BC)", "(DE)", "(HL)", "(C)"], [0x0A, 0x1A, 0x7E, 0xF2], [0x02, 0x12, 0x77, 0xE2]);
 		this.A_AddressingMode(["(HLD)", "(HL-)"], [0x3A, 0x3A], [0x32, 0x32]);
@@ -59,17 +64,15 @@ export class AsmZ80_GB implements IAsmPlatform {
 		Platform.AddInstruction("LD", { addressingMode: "A,([exp])", opCode: [, , 0xFA] });
 		Platform.AddInstruction("LD", { addressingMode: "A,[exp]", opCode: [, 0x3E] });
 
-		{
-			const addressingMode = ["AF", "BC", "DE", "HL"];
-			const opCode1 = [0xF5, 0xC5, 0xD5, 0xE5];
-			const opCode2 = [0xF1, 0xC1, 0xD1, 0xE1];
-			for (let i = 0; i < addressingMode.length; ++i) {
-				Platform.AddInstruction("PUSH", { addressingMode: addressingMode[i], opCode: [opCode1[i]] });
-				Platform.AddInstruction("POP", { addressingMode: addressingMode[i], opCode: [opCode2[i]] });
-			}
+		addressingMode = ["AF", "BC", "DE", "HL"];
+		opCode1 = [0xF5, 0xC5, 0xD5, 0xE5];
+		opCode2 = [0xF1, 0xC1, 0xD1, 0xE1];
+		for (let i = 0; i < addressingMode.length; ++i) {
+			Platform.AddInstruction("PUSH", { addressingMode: addressingMode[i], opCode: [opCode1[i]] });
+			Platform.AddInstruction("POP", { addressingMode: addressingMode[i], opCode: [opCode2[i]] });
 		}
 
-		this.StartToA("ADD", 0x80, 0xC6);
+
 		this.StartToA("ADC", 0x88, 0xCE);
 		this.StartToA("SUB", 0x90, 0xD6);
 		this.StartToA("SBC", 0x98, 0xDE);
@@ -86,6 +89,7 @@ export class AsmZ80_GB implements IAsmPlatform {
 		this.Single("DEC", ["BC", "DE", "HL", "SP"], [0x0B, 0x1B, 0x2B, 0x3B]);
 
 		this.Combine("ADD", "HL", ["BC", "DE", "HL", "SP"], [0x09, 0x19, 0x29, 0x39]);
+		this.StartToA("ADD", 0x80, 0xC6);
 		Platform.AddInstruction("ADD", { addressingMode: "SP,[exp]", opCode: [, 0xE8] });
 
 		this.Single("SWAP", ["B", "C", "D", "E", "H", "L", "(HL)", "A"], [0x30CB, 0x31CB, 0x32CB, 0x33CB, 0x34CB, 0x35CB, 0x36CB, 0x37CB]);
@@ -143,23 +147,25 @@ export class AsmZ80_GB implements IAsmPlatform {
 			Platform.AddInstruction("LD", { addressingMode: `${modes[i]},A`, opCode: [, opCode2[i]] });
 	}
 
-	private StartToA(ins: string, start: number, last?: number) {
+	private StartToA(ins: string, start: number, last: number) {
 		const modes = ["B", "C", "D", "E", "H", "L", "(HL)", "A", "[exp]"];
 		for (let i = 0; i < modes.length; ++i) {
-			if (i !== modes.length - 1)
+			if (i !== modes.length - 1) {
 				Platform.AddInstruction(ins, { addressingMode: `A,${modes[i]}`, opCode: [start++] });
-			else if (last !== undefined)
-				Platform.AddInstruction(ins, { addressingMode: "A,[exp]", opCode: [, last] });
+			} else {
+				Platform.AddInstruction(ins, { addressingMode: "[exp]", opCode: [, last] });
+			}
 		}
 	}
 
-	private OpCodeOneIncrement(ins: string, start: number, last?: number) {
+	private OpCodeOneIncrement(ins: string, start: number, last: number) {
 		const modes = ["B", "C", "D", "E", "H", "L", "(HL)", "A", "[exp]"];
 		for (let i = 0; i < modes.length; ++i) {
-			if (i !== modes.length - 1)
+			if (i !== modes.length - 1) {
 				Platform.AddInstruction(ins, { addressingMode: modes[i], opCode: [start++] });
-			else if (last !== undefined)
+			} else {
 				Platform.AddInstruction(ins, { addressingMode: "[exp]", opCode: [, last] });
+			}
 		}
 	}
 
